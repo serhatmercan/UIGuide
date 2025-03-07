90ca2e20d9a9a94cd0e4c92ee77a086b93687050
Serhat Mercan <serhatmercan67@gmail.com>
2025-03-07 21:45:30 +0300

    Revision | 070325 |

--- TOOLS/MESSAGEBOX/MessageBox.js
+++ TOOLS/MESSAGEBOX/MessageBox.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"com/serhatmercan/controller/BaseController",
+	"sap/m/FormattedText",
 	"sap/m/MessageBox",
 	"sap/ui/model/json/JSONModel"
-], (BaseController, MessageBox, JSONModel) => {
+], (BaseController, FormattedText, MessageBox, JSONModel) => {
 	"use strict";
 
 	return BaseController.extend("com.serhatmercan.Controller", {
@@ -24,13 +25,19 @@ sap.ui.define([
 			const oMBAction = MessageBox.Action;
 			const oMBIcon = MessageBox.Icon;
 			const oMBIconQuestion = oMBIcon.QUESTION;
+			const sInfoMessage = this.getText("infoMessage"); // infoMessage(i18n): First Line.<br>Second Line Begin <strong>Stop</strong> Second Line End.<br>Third Line.
+			const oInfoMessageFT = new FormattedText("InfoMsgFT", {
+				htmlText: sInfoMessage
+			});
 
+			// Confirmation
 			MessageBox.confirm(this.getText("Confirm"), {
 				onClose: (sAction) => {
 					if (sAction === oMBAction.OK) { }
 				}
 			});
 
+			// Default
 			MessageBox.show("", {
 				actions: [oMBAction.CLOSE],
 				icon: oMBIcon.ERROR,
@@ -38,6 +45,15 @@ sap.ui.define([
 				title: "Message"
 			});
 
+			// Information w/ Formatted
+			MessageBox.show(oInfoMessageFT, {
+				actions: [oMBAction.OK],
+				defaultAction: oMBAction.OK,
+				icon: oMBIcon.INFORMATION,
+				title: "Information"
+			});
+
+			// Warning
 			MessageBox.warning(this.getText("Warning"), {
 				actions: [oMBAction.OK, oMBAction.CANCEL],
 				emphasizedAction: oMBAction.OK,
